d767e1ccbf77fcb98e4fd773e5cd462b9b8bcee6
lasya2206 <94067773+lasya2206@users.noreply.github.com>
2022-02-19 22:15:49 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,21 @@
+var findMin = function(nums) {
+    let min = nums[0]
+for ( let i = 0 ; i < nums.length; i++){
+if ( nums[i] < min) min = nums[i]
+}
+return min
+    
+};
+var searchRange = function(N, T) {
+    const find = (target, arr, left=0, right=arr.length) => {
+        while (left <= right) {
+            let mid = left + right >> 1
+            if (arr[mid] < target) left = mid + 1
+            else right = mid - 1
+        }
+        return left
+    } 
+    let Tleft = find(T, N)
+    if (N[Tleft] !== T) return [-1,-1]
+    return [Tleft, find(T+1, N, Tleft) - 1]
+};
